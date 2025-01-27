481deb7c822f579fa9e93961fb34da11d3aa8ff2
Alon A. Rabinowitz <alonrab@outlook.com>
2025-01-27 12:19:39 +0200

    Using built in music notes, copied from tutorial.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BUZZER_PIN 5
+#define BUZZER_CHANNEL 3
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  // pinMode(BUZZER_PIN, OUTPUT);
+  ledcAttachPin(BUZZER_PIN, BUZZER_CHANNEL);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  ledcWriteNote(BUZZER_CHANNEL, NOTE_C, 4);
+  delay(500);
+  ledcWriteNote(BUZZER_CHANNEL, NOTE_D, 4);
+  delay(500);
+  ledcWriteNote(BUZZER_CHANNEL, NOTE_E, 4);
+  delay(500);
+  ledcWriteNote(BUZZER_CHANNEL, NOTE_F, 4);
+  delay(500);
+  ledcWriteNote(BUZZER_CHANNEL, NOTE_G, 4);
+  delay(500);
+  ledcWriteNote(BUZZER_CHANNEL, NOTE_A, 4);
+  delay(500);
+  ledcWriteNote(BUZZER_CHANNEL, NOTE_B, 4);
+  delay(500);
 }
